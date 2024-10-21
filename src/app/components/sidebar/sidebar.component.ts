2954d3010dbc383a169589be159a976b823fc201
AnnaBLoz <anninha_beatriz@terra.com.br>
2024-10-21 19:14:24 -0300

    TOK-27 Eu usuário desejo realizar login na plataforma

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    this.router.navigate(['/login']);
+  }
+}
